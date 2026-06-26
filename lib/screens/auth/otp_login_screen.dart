673afcbd50f277d5f71c2ca36e4d149c7096d5ae
Beryl <berylrafi3@gmail.com>
2026-06-26 11:49:22 +0700

    bikin otp login

---
+++ lib/screens/auth/otp_login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:otp/otp.dart';
+import '../home/wallet_home_screen.dart';
+
+class OtpLoginScreen extends StatefulWidget {
+  final String uid;
+  final String secret;
+
+  const OtpLoginScreen({super.key, required this.uid, required this.secret});
+
+  @override
+  State<OtpLoginScreen> createState() => _OtpLoginScreenState();
+}
+
+class _OtpLoginScreenState extends State<OtpLoginScreen> {
+  final otpController = TextEditingController();
+
+  bool loading = false;
+
+  Future<void> verifyOtp() async {
+    setState(() {
+      loading = true;
+    });
+  }
+}
